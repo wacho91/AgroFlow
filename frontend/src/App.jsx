9c099ffcce110e51d6e45bedc14def036e0b6ebd
Agencia IA <agencia@ia.com>
2026-09-24 11:24:07 -0500

    Update file App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import InsumosPage from './pages/InsumosPage';
 import CultivosPage from './pages/CultivosPage';
 import TesoreriaPage from './pages/TesoreriaPage';
 import EventosPage from './pages/EventosPage';
+import JornalerosPage from './pages/JornalerosPage';
 
 export default function App() {
   return (
@@ -23,6 +24,7 @@ export default function App() {
           <Route path="insumos" element={<InsumosPage />} />
           <Route path="cultivos" element={<CultivosPage />} />
           <Route path="tesoreria" element={<TesoreriaPage />} />
+          <Route path="jornaleros" element={<JornalerosPage />} />
           <Route path="eventos" element={<EventosPage />} />
         </Route>
         
